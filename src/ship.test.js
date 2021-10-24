82d244f030969e70d1110b5dd148995dd3c17e68
steven <steven.h@live.com>
2021-10-24 13:32:04 -0700

    add tests of ship's properties and methods

---
+++ src/ship.test.js
@@ -0,0 +1,31 @@
+import Ship from "./ship";
+
+let ship;
+beforeEach(() => {
+    ship = new Ship(5);
+})
+
+describe("constructor", () => {
+    it("should take a length", () => {
+        expect(ship.length).toBe(5);
+    })
+})
+
+describe("hit method", () => {
+    it("should take a number and mark that position as hit", () => {
+        ship.hit([4, 5]);
+        expect(ship.hits.get("4-5")).toEqual([4, 5]);
+    })
+})
+
+describe("isSunk() method", () => {
+    it("should return boolean by calculating the length and positions hit", () => {
+        ship.hit([1, 1]);
+        ship.hit([1, 2]);
+        ship.hit([1, 3]);
+        ship.hit([1, 4]);
+        ship.hit([1, 5]);
+        ship.isSunk();
+        expect(ship.sunk).toBeTruthy();
+    })
+})
