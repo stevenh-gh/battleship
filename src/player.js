715b0eb26be4f8abfc8d8d2e545cd86bf72b0e78
steven <steven.h@live.com>
2021-11-04 18:57:43 -0700

    refactor

--- src/player.js
+++ src/player.js
@@ -6,26 +6,33 @@ export default class Player {
     }
     attack(guess) {
         if (guess) {
-            if (this.checkDuplicate(guess)) {
-                throw `${guess} has already been guessed!`
-            }
-            this.makeAttack(guess);
+            this.ifPlayer(guess);
         } else if (this.isComputer === false) {
             throw "player must provide guess coordinates";
         } else {
-            let loop = true;
-            while (loop) {
-                let x = Math.floor(Math.random() * 10);
-                let y = Math.floor(Math.random() * 10);
-                let guess = [x, y];
-                if (this.checkDuplicate(guess)) {
-                    continue;
-                } else {
-                    this.makeAttack(guess);
-                    loop = false;
-                }
+            this.ifComputer();
+        }
+    }
+    ifComputer() {
+        let loop = true;
+        while (loop) {
+            let x = Math.floor(Math.random() * 10);
+            let y = Math.floor(Math.random() * 10);
+            let guess = [x, y];
+            if (this.checkDuplicate(guess)) {
+                continue;
+            } else {
+                this.makeAttack(guess);
+                loop = false;
             }
         }
+    }
+
+    ifPlayer(guess) {
+        if (this.checkDuplicate(guess)) {
+            throw `${guess} has already been guessed!`;
+        }
+        this.makeAttack(guess);
     }
 
     makeAttack(guess) {
