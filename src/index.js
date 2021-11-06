5e4b02c69dfcf8b51cc9ce489e4985006fbd56b5
steven <steven.h@live.com>
2021-11-05 17:33:59 -0700

    add grid

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+let placeShips = document.querySelector("#place-ships");
+for (let i = 0; i < 10; ++i) {
+    for (let j = 0; j < 10; ++j) {
+        let div = document.createElement("div");
+        div.classList.add(...
+            "border border-black".split(" "));
+        div.innerText = `${i},${j}`;
+        div.id = `${i}-${j}`
+        placeShips.appendChild(div);
+    }
+}
