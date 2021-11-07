c31e4104106b974aa94b98088b05b4eedc5ace61
steven <steven.h@live.com>
2021-11-07 14:51:22 -0800

    move for loop into try so that when error occurs, it does not mark positions

--- src/index.js
+++ src/index.js
@@ -25,23 +25,19 @@ let count = 0;
 let gameboard = new Gameboard();
 placeShips.addEventListener("click", e => {
     let coord = e.target.id.split("-").map(c => parseInt(c));
-    for (let i = 0; i < lengths[count]; ++i) {
-        try {
+    try {
+        rotate ? gameboard.placeShip(new Ship(lengths[count]), coord, rotate) : gameboard.placeShip(new Ship(lengths[count]), coord);
+        console.log(gameboard);
+        for (let i = 0; i < lengths[count]; ++i) {
             if (rotate) {
                 document.getElementById(`${coord[0]+i}-${coord[1]}`).classList.add("bg-blue-300");
             } else {
                 document.getElementById(`${coord[0]}-${coord[1]+i}`).classList.add("bg-blue-300");
             }
-        } catch {
-            alert("invalid position");
         }
-    }
-    try {
-        rotate ? gameboard.placeShip(new Ship(lengths[count]), coord, rotate) : gameboard.placeShip(new Ship(lengths[count]), coord);
-        console.log(gameboard);
         ++count;
-    } catch {
-        alert("invalid position");
+    } catch (e) {
+        alert(e);
     }
 })
 placeShips.addEventListener("mouseover", e => {
