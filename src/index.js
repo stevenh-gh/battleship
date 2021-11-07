f042d345d7a1ba0763e4dd85a62a413da6c061f3
steven <steven.h@live.com>
2021-11-06 18:17:06 -0700

    add gray bg when hovering over cells

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ for (let i = 0; i < 10; ++i) {
             "border border-black".split(" "));
         div.innerText = `${i},${j}`;
         div.id = `${i}-${j}`
+        div.addEventListener("mouseover", e => {
+            div.classList.add("bg-gray-400");
+        })
+        div.addEventListener("mouseout", e => {
+            div.classList.remove("bg-gray-400");
+        })
         placeShips.appendChild(div);
     }
 }
