9baa709617bf5eb9bb169bda9ea6a3f9adf793bf
steven <steven.h@live.com>
2021-11-07 12:34:54 -0800

    add horizontal ship preview of length 5

--- src/index.js
+++ src/index.js
@@ -8,12 +8,20 @@ for (let i = 0; i < 10; ++i) {
             "border border-black".split(" "));
         div.innerText = `${i},${j}`;
         div.id = `${i}-${j}`
-        div.addEventListener("mouseover", e => {
-            div.classList.add("bg-gray-400");
-        })
-        div.addEventListener("mouseout", e => {
-            div.classList.remove("bg-gray-400");
-        })
         placeShips.appendChild(div);
     }
 }
+let lengths = [5, 4, 3, 3, 2];
+placeShips.addEventListener("mouseover", e => {
+    let coord = e.target.id.split("-").map(c => parseInt(c));
+    placeShips.querySelectorAll("div").forEach(div => {
+        div.classList.remove("bg-gray-400");
+    })
+    for (let i = 0; i < lengths[0]; ++i) {
+        try {
+            document.getElementById(`${coord[0]}-${coord[1]+i}`).classList.add("bg-gray-400");
+        } catch {
+            continue;
+        }
+    }
+})
