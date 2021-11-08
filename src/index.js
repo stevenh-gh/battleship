8308017572a729b6980591a52b7bcf5d909e6e0f
steven <steven.h@live.com>
2021-11-08 13:53:47 -0800

    refactor event listeners
    remove event listeneners when count reaches 5

--- src/index.js
+++ src/index.js
@@ -23,37 +23,50 @@ rotateBtn.addEventListener("click", () => {
 let lengths = [5, 4, 3, 3, 2];
 let count = 0;
 let gameboard = new Gameboard();
-placeShips.addEventListener("click", e => {
+placeShips.addEventListener("click", handleClicks)
+placeShips.addEventListener("mouseover", handleMouseOver)
+
+
+function placeShipsRmEvnt() {
+    if (count === 5) {
+        placeShips.removeEventListener("click", handleClicks);
+        placeShips.removeEventListener("mouseover", handleMouseOver);
+    }
+}
+
+function handleMouseOver(e) {
+    let coord = e.target.id.split("-").map(c => parseInt(c));
+    placeShips.querySelectorAll("div").forEach(div => {
+        div.classList.remove("bg-gray-400");
+    });
+    for (let i = 0; i < lengths[count]; ++i) {
+        try {
+            if (rotate) {
+                document.getElementById(`${coord[0] + i}-${coord[1]}`).classList.add("bg-gray-400");
+            } else {
+                document.getElementById(`${coord[0]}-${coord[1] + i}`).classList.add("bg-gray-400");
+            }
+        } catch {
+            continue;
+        }
+    }
+}
+
+function handleClicks(e) {
     let coord = e.target.id.split("-").map(c => parseInt(c));
     try {
         rotate ? gameboard.placeShip(new Ship(lengths[count]), coord, rotate) : gameboard.placeShip(new Ship(lengths[count]), coord);
         console.log(gameboard);
         for (let i = 0; i < lengths[count]; ++i) {
             if (rotate) {
-                document.getElementById(`${coord[0]+i}-${coord[1]}`).classList.add("bg-blue-300");
+                document.getElementById(`${coord[0] + i}-${coord[1]}`).classList.add("bg-blue-300");
             } else {
-                document.getElementById(`${coord[0]}-${coord[1]+i}`).classList.add("bg-blue-300");
+                document.getElementById(`${coord[0]}-${coord[1] + i}`).classList.add("bg-blue-300");
             }
         }
         ++count;
+        placeShipsRmEvnt();
     } catch (e) {
         alert(e);
     }
-})
-placeShips.addEventListener("mouseover", e => {
-    let coord = e.target.id.split("-").map(c => parseInt(c));
-    placeShips.querySelectorAll("div").forEach(div => {
-        div.classList.remove("bg-gray-400");
-    })
-    for (let i = 0; i < lengths[count]; ++i) {
-        try {
-            if (rotate) {
-                document.getElementById(`${coord[0]+i}-${coord[1]}`).classList.add("bg-gray-400");
-            } else {
-                document.getElementById(`${coord[0]}-${coord[1]+i}`).classList.add("bg-gray-400");
-            }
-        } catch {
-            continue;
-        }
-    }
-})
+}
