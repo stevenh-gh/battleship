b34b7d0c256f47c379c6c49b92047cba873e0129
steven <steven.h@live.com>
2021-11-10 20:03:17 -0800

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -66,85 +66,107 @@ function handleClicks(e) {
         }
         ++count;
         if (count === 5) {
-            placeShipsRmEvnt();
-            document.getElementById("place-ships-div").classList.add("hidden");
-            document.getElementById("gameplay-div").classList.remove("hidden");
-            // to setup 2 gameboards:
-            // combine all the ships' coords into a single array
-            let playerShipsCoords = [];
-            gameboard.ships.forEach(ship => {
-                playerShipsCoords = playerShipsCoords.concat(ship.coord);
-            })
-
-            // and when constructing grid, if grid num matches a coord, color it
-            let playerGrid = document.getElementById("player-game-grid");
-            for (let i = 0; i < 10; ++i) {
-                for (let j = 0; j < 10; ++j) {
-                    let cell = document.createElement("div");
-                    cell.innerText = `${i},${j}`;
-                    cell.id = `gg-${i}-${j}`;
-                    if (playerShipsCoords.some(ele => ele[0] === i && ele[1] === j)) {
-                        cell.classList.add("bg-blue-500");
-                    }
-                    playerGrid.appendChild(cell);
-                }
-            }
-            // use random numbers to generate ships for enemy gameboard
-            // make ships in arr
-            let comShips = [new Ship(5), new Ship(4), new Ship(3), new Ship(3), new Ship(2)];
-            let comShipsCount = 0;
-            while (comShipsCount < comShips.length) {
-                // generate random nums for x,y
-                let x = Math.floor(Math.random() * 10);
-                let y = Math.floor(Math.random() * 10);
-                let v = Math.floor(Math.random() * 2);
-                let coord = [x, y];
-                // placeships with try/catch
-                try {
-                    enemyGameboard.placeShip(comShips[comShipsCount], coord, v);
-                    // increment
-                    ++comShipsCount;
-                } catch {
-                    continue;
-                }
-                console.log("enemy");
-                console.log(enemyGameboard);
-            }
-            // draw enemy board
-            let comGameGrid = document.getElementById("com-game-grid");
-            for (let i = 0; i < 10; ++i) {
-                for (let j = 0; j < 10; ++j) {
-                    let cell = document.createElement("div");
-                    cell.innerText = `${i},${j}`;
-                    cell.id = `eg-${i}-${j}`;
-                    comGameGrid.appendChild(cell);
-                }
-            }
-            // -------gameplay loop---------
-            let player = new Player(enemyGameboard);
-            let computer = new Player(gameboard, true);
-            // -----------------------------
-            comGameGrid.addEventListener("click", e => {
-                let id = e.target.id.split("-");
-                let atk = [id[1], id[2]].map(c => parseInt(c));
-                // console.log(player.attack(atk));
-                if (player.attack(atk)) {
-                    document.getElementById(e.target.id).classList.add("bg-red-400");
-                } else {
-                    document.getElementById(e.target.id).classList.add("bg-gray-400");
-                }
-                let cAtk = computer.attack();
-                let cAtkCoord = cAtk[1];
-                if (cAtk[0]) {
-                    document.getElementById(`gg-${cAtkCoord[0]}-${cAtkCoord[1]}`).classList.remove("bg-blue-500");
-                    document.getElementById(`gg-${cAtkCoord[0]}-${cAtkCoord[1]}`).classList.add("bg-red-400");
-                } else {
-                    document.getElementById(`gg-${cAtkCoord[0]}-${cAtkCoord[1]}`).classList.add("bg-gray-400");
-                }
-                console.log(enemyGameboard.isAllSunk() || gameboard.isAllSunk());
-            })
+            gameStart();
         }
     } catch (e) {
         alert(e);
+    }
+}
+
+function gameStart() {
+    placeShipsRmEvnt();
+    document.getElementById("place-ships-div").classList.add("hidden");
+    document.getElementById("gameplay-div").classList.remove("hidden");
+    // to setup 2 gameboards:
+    // combine all the ships' coords into a single array
+    let playerShipsCoords = [];
+    gameboard.ships.forEach(ship => {
+        playerShipsCoords = playerShipsCoords.concat(ship.coord);
+    })
+
+    // and when constructing grid, if grid num matches a coord, color it
+    drawPlayerBoard(playerShipsCoords);
+    // use random numbers to generate ships for enemy gameboard
+    // make ships in arr
+    let comShips = [new Ship(5), new Ship(4), new Ship(3), new Ship(3), new Ship(2)];
+    let comShipsCount = 0;
+    while (comShipsCount < comShips.length) {
+        // generate random nums for x,y
+        let x = Math.floor(Math.random() * 10);
+        let y = Math.floor(Math.random() * 10);
+        let v = Math.floor(Math.random() * 2);
+        let coord = [x, y];
+        // placeships with try/catch
+        try {
+            enemyGameboard.placeShip(comShips[comShipsCount], coord, v);
+            // increment
+            ++comShipsCount;
+        } catch {
+            continue;
+        }
+        console.log("enemy");
+        console.log(enemyGameboard);
+    }
+    // draw enemy board
+    let comGameGrid = drawEnemyBoard();
+    // -------gameplay loop---------
+    let player = new Player(enemyGameboard);
+    let computer = new Player(gameboard, true);
+    // -----------------------------
+    let doComGameGridClick = e => handleComGameGridClick(e, player, computer);
+    comGameGrid.addEventListener("click", doComGameGridClick);
+}
+
+function checkWin(comGameGrid, doComGameGridClick) {
+    if (enemyGameboard.isAllSunk() || gameboard.isAllSunk()) {
+        enemyGameboard.isAllSunk() ? alert("player wins!") : alert("computer wins!");
+        comGameGrid.removeEventListener("click", doComGameGridClick);
+    }
+}
+
+function drawPlayerBoard(playerShipsCoords) {
+    let playerGrid = document.getElementById("player-game-grid");
+    for (let i = 0; i < 10; ++i) {
+        for (let j = 0; j < 10; ++j) {
+            let cell = document.createElement("div");
+            cell.innerText = `${i},${j}`;
+            cell.id = `gg-${i}-${j}`;
+            if (playerShipsCoords.some(ele => ele[0] === i && ele[1] === j)) {
+                cell.classList.add("bg-blue-500");
+            }
+            playerGrid.appendChild(cell);
+        }
+    }
+}
+
+function drawEnemyBoard() {
+    let comGameGrid = document.getElementById("com-game-grid");
+    for (let i = 0; i < 10; ++i) {
+        for (let j = 0; j < 10; ++j) {
+            let cell = document.createElement("div");
+            cell.innerText = `${i},${j}`;
+            cell.id = `eg-${i}-${j}`;
+            comGameGrid.appendChild(cell);
+        }
+    }
+    return comGameGrid;
+}
+
+function handleComGameGridClick(e, player, computer) {
+    let id = e.target.id.split("-");
+    let atk = [id[1], id[2]].map(c => parseInt(c));
+    // console.log(player.attack(atk));
+    if (player.attack(atk)) {
+        document.getElementById(e.target.id).classList.add("bg-red-400");
+    } else {
+        document.getElementById(e.target.id).classList.add("bg-gray-400");
+    }
+    let cAtk = computer.attack();
+    let cAtkCoord = cAtk[1];
+    if (cAtk[0]) {
+        document.getElementById(`gg-${cAtkCoord[0]}-${cAtkCoord[1]}`).classList.remove("bg-blue-500");
+        document.getElementById(`gg-${cAtkCoord[0]}-${cAtkCoord[1]}`).classList.add("bg-red-400");
+    } else {
+        document.getElementById(`gg-${cAtkCoord[0]}-${cAtkCoord[1]}`).classList.add("bg-gray-400");
     }
 }
